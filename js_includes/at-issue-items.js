86878d4d05264fe5929813972921e393d24b6795
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2018-11-28 16:41:11 +0100

    Update at-issue-items.js

--- js_includes/at-issue-items.js
+++ js_includes/at-issue-items.js
@@ -3,23 +3,11 @@ var shuffleSequence = seq(
                              "consent",
                             //"instructions",
                               "story", "test",
-                             //"finished",
+                             //"completed",
                              "subj_info", "sr", "code"
                             );
 
 var defaults = [
-    "Separator", {
-        transfer: 1000,
-        normalMessage: "Please wait for the next sentence.",
-        errorMessage: "Wrong. Please wait for the next sentence."
-    },
-    "Message", {
-        hideProgressBar: false,
-        transfer: "click",
-    },
-    "Question", {
-        hideProgressBar: false,
-    },
     "Form", {
         hideProgressBar: false,
         continueOnReturn: true,
@@ -34,19 +22,23 @@ var items = [
 
     ["sr", "__SendResults__", { }],
 
-    ["sep", "Separator", { }],
-
     ["setcounter", "__SetCounter__", { }],
 
     //["instructions", "Form", {html: {include: 'instructions.html'}}],
 
     //["completed", "Form", {html: {include: 'completion_message.html'}}],
 
-    [["story", 1], "Form", {html: {include: 'John_bomb_belief_story_pillow.html'}}],
-    [["story", 1], "Form", {html: {include: 'John_bomb_fact_story_pillow.html'}}],
+    //[["story", 1], "Form", {html: {include: 'John_bomb_belief_story_pillow.html'}}],
+    //[["story", 1], "Form", {html: {include: 'John_bomb_fact_story_pillow.html'}}],
 
-    [["story", 2], "Form", {html: {include: 'Mary_bomb_belief_story_pillow.html'}}],
-    [["story", 2], "Form", {html: {include: 'Mary_bomb_fact_story_pillow.html'}}],
+    //[["story", 2], "Form", {html: {include: 'Mary_bomb_belief_story_pillow.html'}}],
+    //[["story", 2], "Form", {html: {include: 'Mary_bomb_fact_story_pillow.html'}}],
+
+    ["story", "Form", {html: {include: 'John_bomb_belief_story_pillow.html'}}],
+    ["story", "Form", {html: {include: 'John_bomb_fact_story_pillow.html'}}],
+
+    ["story", "Form", {html: {include: 'Mary_bomb_belief_story_pillow.html'}}],
+    ["story", "Form", {html: {include: 'Mary_bomb_fact_story_pillow.html'}}],
 
     //[["story", 1], "Form", {html: {include: 'John_bomb_belief_story_bomb.html'}}],
     //[["story", 1], "Form", {html: {include: 'John_bomb_fact_story_bomb.html'}}],
@@ -54,11 +46,17 @@ var items = [
     //[["story", 2], "Form", {html: {include: 'Mary_bomb_belief_story_bomb.html'}}],
     //[["story", 2], "Form", {html: {include: 'Mary_bomb_fact_story_bomb.html'}}],
 
-    [["test", [3,1]], "Form", html: {include: 'John_fact_question.html'}}],
-    [["test", [3,1]], "Form", html: {include: 'John_belief_question.html'}}],
+    //[["test", [3,1]], "Form", html: {include: 'John_fact_question.html'}}],
+    //[["test", [3,1]], "Form", html: {include: 'John_belief_question.html'}}],
 
-    [["test", [4,2]], "Form", html: {include: 'Mary_fact_question.html'}}],
-    [["test", [4,2]], "Form", html: {include: 'Mary_belief_question.html'}}],
+    //[["test", [4,2]], "Form", html: {include: 'Mary_fact_question.html'}}],
+    //[["test", [4,2]], "Form", html: {include: 'Mary_belief_question.html'}}],
+
+    ["test", "Form", html: {include: 'John_fact_question.html'}}],
+    ["test", "Form", html: {include: 'John_belief_question.html'}}],
+
+    ["test", "Form", html: {include: 'Mary_fact_question.html'}}],
+    ["test", "Form", html: {include: 'Mary_belief_question.html'}}],
 
     ["consent", "Form", {html: {include: 'consent.html'}}],
 
