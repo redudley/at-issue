aebdf076eed6d70effa55c360c280be4077a30c9
Rachel Dudley <rachel.elaine.dudley@gmail.com>
2018-11-30 20:52:19 +0100

    updated with results from pilot and new dispatcher for full study

---
+++ dispatcher-ATI-full.php
@@ -0,0 +1,18 @@
+<?php
+// Get a global counter from server and update counter
+$counter_file = "/tmp/dispatch-counter-ATI-full";
+$counter_val = file_get_contents($counter_file);
+file_put_contents($counter_file, $counter_val + 1);
+
+// Possible redirections
+$redirects = array("http://spellout.net/ibexexps/redudley/ATI-05/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-06/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-07/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-08/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-09/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-10/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-11/experiment.html",
+                   "http://spellout.net/ibexexps/redudley/ATI-12/experiment.html",);
+// Redirect user to the next link
+header("Location: " . $redirects[$counter_val % count($redirects)]);
+?>
